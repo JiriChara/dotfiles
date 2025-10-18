df45888112cd3df752b37a213bec5664be6426c2
Jiri Chara <jiri.chara@pm.me>
2025-10-18 08:20:57 +0200

    Disable luarocks for now

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  rocks = { enabled = false }
 })
 
 vim.cmd('source $HOME/.config/nvimrc')
