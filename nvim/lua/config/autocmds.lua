5493f2930f8c30d6ae162b8787332690eb303efb
Jiri Chara <jiri.chara@pm.me>
2025-12-05 10:23:54 +0100

    Make diagnostics window not focusable

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -61,7 +61,7 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
   callback = function()
     vim.diagnostic.open_float({
       scope = "cursor",
-      focusable = true,
+      focusable = false,
     })
   end,
 })
