c7e712ae9bb2394f25736f1828498a402ca038e7
Jiri Chara <jiri.chara@pm.me>
2026-01-06 20:13:55 +0100

    Fix treesitter config path

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -71,7 +71,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter.config").setup({
         highlight = {
           enable = true,
         },
