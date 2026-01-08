d7aad107d71752d95d0b2d84a072fca618aa2f65
Jiri Chara <jiri.chara@pm.me>
2026-01-08 21:05:14 +0100

    Update Vim plugins

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -35,7 +35,6 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
